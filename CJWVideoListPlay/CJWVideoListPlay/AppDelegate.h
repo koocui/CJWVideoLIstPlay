ea35de2d2b472249697cf349945830b8448235f0
小崔 <xiaocui@xiaocuideMacBook-Pro.local>
2018-04-18 11:42:02 +0800

    创建工程。

---
+++ CJWVideoListPlay/CJWVideoListPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJWVideoListPlay
+//
+//  Created by 小崔 on 2018/4/18.
+//  Copyright © 2018年 CJW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
